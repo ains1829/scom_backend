30b6614af4e250b4cdd7de96491a0b67b44bf0b5
ains1829 <andyrakotonavalona0@gmail.com>
2024-07-12 09:19:16 +0300

    debug role

---
+++ src/main/java/com/ains/myspring/security/error/AccesException.java
@@ -0,0 +1,7 @@
+package com.ains.myspring.security.error;
+
+public class AccesException extends Exception {
+  public AccesException(String messageError) {
+    super(messageError);
+  }
+}
--- src/test/java/com/ains/myspring/MyspringApplicationTests.java
+++ src/test/java/com/ains/myspring/MyspringApplicationTests.java
@@ -1,22 +1,12 @@
 package com.ains.myspring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.ains.myspring.services.AdministrationService;
 
 @SpringBootTest
 class MyspringApplicationTests {
-	@Autowired
-	private AdministrationService _service;
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void TestAdminEmail() {
-		_service.getAdminByEmail("andyrakotonavalona@gmail.com");
 	}
 }
