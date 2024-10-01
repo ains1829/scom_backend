cb02935c9b3bdef2bf221dd2d7cbbda7d5316303
ains1829 <andyrakotonavalona0@gmail.com>
2024-10-01 08:54:15 +0300

    change fault

--- src/main/java/com/ains/myspring/controller/privates/PrivateController.java
+++ src/main/java/com/ains/myspring/controller/privates/PrivateController.java
@@ -1,6 +1,5 @@
 package com.ains.myspring.controller.privates;
 
-import java.sql.Date;
 import java.util.HashMap;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
